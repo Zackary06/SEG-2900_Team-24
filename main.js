dbb07822774328cdbc3b3b026cac02281a56b707
Tony Yu <tonyyu163@gmail.com>
2024-10-25 23:25:48 -0400

    I added identifiers to the login and register button so I could test my event listeners in the main.js file. I also changed the script for index.html from script to main.js, but this can be undone if necessary. Overall, this commit was just to check if I could get js to work with our current page.J'ai ajouté des identifiants au bouton de connexion et d'enregistrement afin de pouvoir tester mes écouteurs d'événements dans le fichier main.js. J'ai également modifié le script pour index.html de script à main.js, mais cela peut être annulé si nécessaire. Dans l’ensemble, ce commit visait simplement à vérifier si je pouvais faire fonctionner js avec notre page actuelle.

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 var a =1;
 var test = "test commit"
 var caseyLi = "Casey Li"
+
+document.getElementById('login_btn').addEventListener('click', function(){
+    alert('CASEY LI!!!! This would bring the user to a login page')
+})
+
+document.getElementById('signup_btn').addEventListener('click', function(){
+    alert('Casey Li. This would bring the user to a signup page')
+})
